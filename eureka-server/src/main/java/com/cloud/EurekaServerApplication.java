58483c35a25ace6b2692655410916088097f956e
daijinming <daijinming90@163.com>
2019-02-22 15:57:34 +0800

    整合eureka

--- eureka-server/src/main/java/com/cloud/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/cloud/EurekaServerApplication.java
@@ -2,20 +2,19 @@ package com.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * Description 首页
  *
  * @Author daijinming
  * @Date 2019/2/21 15:00
  **/
+@EnableEurekaServer
 @SpringBootApplication
-@EnableEurekaClient
-public class CloudManageApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudManageApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
