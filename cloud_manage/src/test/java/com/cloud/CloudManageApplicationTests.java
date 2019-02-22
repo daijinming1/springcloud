9ed4f2038ec1f06f4fa81dba981e96e6a6879c9e
daijinming <daijinming90@163.com>
2019-02-22 11:01:02 +0800

    数据库加密操作

--- cloud_manage/src/test/java/com/cloud/CloudManageApplicationTests.java
+++ cloud_manage/src/test/java/com/cloud/CloudManageApplicationTests.java
@@ -1,16 +1,30 @@
 package com.cloud;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CloudManageApplicationTests {
+    //根据上面配的盐值自动注入
+    @Autowired
+    StringEncryptor stringEncryptor;
+
+    @Test
+    public void encryptPwd() {
+        String password="password";//要加密的密码
+        String result = stringEncryptor.encrypt(password);
+        //打印结果
+        System.out.println(result);
+    }
 
 	@Test
 	public void contextLoads() {
+		System.out.println("-----------");
 	}
 
 }
